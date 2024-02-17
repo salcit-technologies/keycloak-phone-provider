62529937d96acfb26e70b28d2ef3d69e04478d18
baswaraj mamidgi <info@salcit.in>
2024-02-17 22:30:08 +0530

    keycloak 23.0.4 migration

--- docker/build-template.sh
+++ docker/build-template.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "test" ] || [[ @project.version@ = *"snapshot" ]] || [[ @project.version@ = *"SNAPSHOT" ]]
 then
-  docker build -t coopersoft/keycloak:@version.keycloak@_phone-@project.version@ .
+  docker build -t swaasa/keycloak:@version.keycloak@_phone-@project.version@ .
 else
-  docker buildx build . --platform linux/amd64,linux/arm64 --push -t coopersoft/keycloak:@version.keycloak@_phone-@project.version@
+  docker buildx build . --platform linux/amd64,linux/arm64 --push -t swaasa/keycloak:@version.keycloak@_phone-@project.version@
 fi
--- keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/forms/RegistrationPhoneUserCreation.java
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/forms/RegistrationPhoneUserCreation.java
@@ -24,7 +24,7 @@ import org.keycloak.userprofile.UserProfileContext;
 import org.keycloak.userprofile.UserProfileProvider;
 import org.keycloak.userprofile.ValidationException;
 
-import javax.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.MultivaluedMap;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -235,7 +235,7 @@ public class RegistrationPhoneUserCreation implements FormActionFactory, FormAct
     }
 
     UserProfileProvider profileProvider = session.getProvider(UserProfileProvider.class);
-    UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION_USER_CREATION, formData);
+    UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION, formData);
 
     String username = profile.getAttributes().getFirstValue(UserModel.USERNAME);
     context.getEvent().detail(Details.USERNAME, username);
@@ -311,7 +311,7 @@ public class RegistrationPhoneUserCreation implements FormActionFactory, FormAct
     }
 
     UserProfileProvider profileProvider = session.getProvider(UserProfileProvider.class);
-    UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION_USER_CREATION, formData);
+    UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION, formData);
     UserModel user = profile.create();
 
 //    UserModel user = context.getSession().users().addUser(context.getRealm(), username);
